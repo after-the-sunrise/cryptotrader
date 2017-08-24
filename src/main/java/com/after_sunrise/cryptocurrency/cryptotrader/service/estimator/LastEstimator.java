660bc1499e00328397486f231c679b818a737a2b
Takanori Takase <scm@after-sunrise.com>
2017-08-24 23:58:53 +0900

    Applied confidence to last estimator.

--- src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/LastEstimator.java
+++ src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/LastEstimator.java
@@ -3,13 +3,25 @@ package com.after_sunrise.cryptocurrency.cryptotrader.service.estimator;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context.Key;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Estimator;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Trade;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Trader.Request;
+import com.google.common.annotations.VisibleForTesting;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Optional;
 
 import static java.math.BigDecimal.ONE;
 import static java.math.BigDecimal.ZERO;
+import static java.math.RoundingMode.HALF_UP;
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.lang3.math.NumberUtils.LONG_ONE;
 
 /**
  * @author takanori.takase
@@ -20,6 +32,8 @@ public class LastEstimator implements Estimator {
 
     private static final Estimation BAIL = Estimation.builder().confidence(ZERO).build();
 
+    private static final Comparator<Trade> COMPARATOR = Comparator.comparing(Trade::getTimestamp).reversed();
+
     @Override
     public String get() {
         return getClass().getSimpleName();
@@ -28,23 +42,57 @@ public class LastEstimator implements Estimator {
     @Override
     public Estimation estimate(Context context, Request request) {
 
-        if (context == null) {
+        Instant from = getNow().minus(LONG_ONE, DAYS);
+
+        Optional<Trade> value = ofNullable(context.listTrades(Key.from(request), from))
+                .orElse(emptyList()).stream()
+                .filter(Objects::nonNull)
+                .filter(t -> Objects.nonNull(t.getTimestamp()))
+                .filter(t -> Objects.nonNull(t.getPrice()))
+                .sorted(COMPARATOR)
+                .findFirst();
+
+        if (!value.isPresent()) {
             return BAIL;
         }
 
-        Key key = Key.from(request);
+        Instant time = value.get().getTimestamp();
 
-        if (!Key.isValid(key)) {
-            return BAIL;
-        }
+        BigDecimal confidence = calculateConfidence(time, getNow());
 
-        BigDecimal price = context.getLastPrice(key);
+        BigDecimal price = value.get().getPrice();
 
-        BigDecimal confidence = price == null ? ZERO : ONE;
-
-        log.debug("Estimated : {} - {}", price, key);
+        log.debug("Estimated : {} (confidence=[{}] time=[{}])", price, confidence, time);
 
         return Estimation.builder().price(price).confidence(confidence).build();
+
+    }
+
+    @VisibleForTesting
+    Instant getNow() {
+        return Instant.now();
+    }
+
+    @VisibleForTesting
+    BigDecimal calculateConfidence(Instant time, Instant now) {
+
+        Duration duration = Duration.between(time, now);
+
+        if (duration.isZero() || duration.isNegative()) {
+            return ONE;
+        }
+
+        // Number from 0 to 29 (= 100 years)
+        double scaled = Math.log(duration.toMillis());
+
+        // Number from 0.00 to 0.79
+        double multiplied = scaled * Math.E / 100;
+
+        // Number from 100.00 to 0.21
+        double confidence = 1 - multiplied;
+
+        // Sanitize in case if +3000 years...
+        return BigDecimal.valueOf(confidence).max(ZERO).setScale(8, HALF_UP);
 
     }
 
--- src/test/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/LastEstimatorTest.java
+++ src/test/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/LastEstimatorTest.java
@@ -3,15 +3,23 @@ package com.after_sunrise.cryptocurrency.cryptotrader.service.estimator;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context.Key;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Estimator.Estimation;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Trade;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Trader.Request;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
 import static java.math.BigDecimal.*;
 import static java.time.Instant.now;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static java.time.temporal.ChronoUnit.*;
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.math.NumberUtils.LONG_ONE;
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 
 /**
  * @author takanori.takase
@@ -28,7 +36,7 @@ public class LastEstimatorTest {
 
         context = mock(Context.class);
 
-        target = new LastEstimator();
+        target = spy(new LastEstimator());
 
     }
 
@@ -40,26 +48,106 @@ public class LastEstimatorTest {
     @Test
     public void testEstimate() throws Exception {
 
+        Instant time = Instant.now();
+        doReturn(time).when(target).getNow();
+        doReturn(TEN).when(target).calculateConfidence(time.minusSeconds(1), time);
+
+        Trade t1 = mock(Trade.class);
+        Trade t2 = mock(Trade.class);
+        Trade t3 = mock(Trade.class);
+        Trade t4 = mock(Trade.class);
+        Trade t5 = mock(Trade.class);
+        when(t1.getTimestamp()).thenReturn(time.minusSeconds(1));
+        when(t2.getTimestamp()).thenReturn(null);
+        when(t3.getTimestamp()).thenReturn(time.minusSeconds(3));
+        when(t4.getTimestamp()).thenReturn(time.minusSeconds(4));
+        when(t5.getTimestamp()).thenReturn(time.minusSeconds(5));
+        when(t1.getPrice()).thenReturn(BigDecimal.valueOf(3));
+        when(t2.getPrice()).thenReturn(BigDecimal.valueOf(4));
+        when(t3.getPrice()).thenReturn(BigDecimal.valueOf(5));
+        when(t4.getPrice()).thenReturn(null);
+        when(t5.getPrice()).thenReturn(BigDecimal.valueOf(2));
+
         Request request = Request.builder().site("s").instrument("i").targetTime(now()).build();
         Key key = Key.from(request);
+        List<Trade> trades = asList(t3, t2, null, t5, null, t1, t4);
+        when(context.listTrades(key, time.minus(LONG_ONE, DAYS))).thenReturn(trades);
 
-        when(context.getLastPrice(key)).thenReturn(TEN);
+        // Estimated
         Estimation result = target.estimate(context, request);
-        assertEquals(result.getPrice(), TEN);
-        assertEquals(result.getConfidence(), ONE);
+        assertEquals(result.getPrice(), t1.getPrice());
+        assertEquals(result.getConfidence(), TEN);
 
-        result = target.estimate(null, request);
-        assertEquals(result.getPrice(), null);
-        assertEquals(result.getConfidence(), ZERO);
-
+        // Invalid Request
         result = target.estimate(context, null);
         assertEquals(result.getPrice(), null);
         assertEquals(result.getConfidence(), ZERO);
 
-        when(context.getLastPrice(key)).thenReturn(null);
+        // Null trades
+        when(context.listTrades(key, time.minus(LONG_ONE, DAYS))).thenReturn(null);
         result = target.estimate(context, request);
         assertEquals(result.getPrice(), null);
         assertEquals(result.getConfidence(), ZERO);
+
+        // Invalid trades
+        when(context.listTrades(key, time.minus(LONG_ONE, DAYS))).thenReturn(asList((Trade) null));
+        result = target.estimate(context, request);
+        assertEquals(result.getPrice(), null);
+        assertEquals(result.getConfidence(), ZERO);
+
+    }
+
+    @Test
+    public void testGetNow() throws InterruptedException {
+
+        Instant t1 = target.getNow();
+
+        Thread.sleep(100L);
+
+        assertNotEquals(target.getNow(), t1);
+
+    }
+
+    @Test
+    public void testCalculateConfidence() {
+
+        Instant now = Instant.now();
+
+        // No diff
+        assertEquals(target.calculateConfidence(now, now), ONE);
+
+        // Future date
+        assertEquals(target.calculateConfidence(now.plusSeconds(1), now), ONE);
+
+        // Seconds
+        assertEquals(target.calculateConfidence(now.minusSeconds(01), now), new BigDecimal("0.81222774"));
+        assertEquals(target.calculateConfidence(now.minusSeconds(10), now), new BigDecimal("0.74963699"));
+        assertEquals(target.calculateConfidence(now.minusSeconds(20), now), new BigDecimal("0.73079530"));
+        assertEquals(target.calculateConfidence(now.minusSeconds(30), now), new BigDecimal("0.71977361"));
+        assertEquals(target.calculateConfidence(now.minusSeconds(40), now), new BigDecimal("0.71195360"));
+        assertEquals(target.calculateConfidence(now.minusSeconds(50), now), new BigDecimal("0.70588793"));
+
+        // Minutes
+        assertEquals(target.calculateConfidence(now.minus(01, MINUTES), now), new BigDecimal("0.70093192"));
+        assertEquals(target.calculateConfidence(now.minus(05, MINUTES), now), new BigDecimal("0.65718286"));
+        assertEquals(target.calculateConfidence(now.minus(15, MINUTES), now), new BigDecimal("0.62731948"));
+        assertEquals(target.calculateConfidence(now.minus(30, MINUTES), now), new BigDecimal("0.60847779"));
+        assertEquals(target.calculateConfidence(now.minus(45, MINUTES), now), new BigDecimal("0.59745610"));
+
+        // Hours
+        assertEquals(target.calculateConfidence(now.minus(1, HOURS), now), new BigDecimal("0.58963610"));
+        assertEquals(target.calculateConfidence(now.minus(4, HOURS), now), new BigDecimal("0.55195271"));
+        assertEquals(target.calculateConfidence(now.minus(8, HOURS), now), new BigDecimal("0.53311101"));
+        assertEquals(target.calculateConfidence(now.minus(16, HOURS), now), new BigDecimal("0.51426932"));
+
+        // Days
+        assertEquals(target.calculateConfidence(now.minus(1, DAYS), now), new BigDecimal("0.50324764"));
+        assertEquals(target.calculateConfidence(now.minus(2, DAYS), now), new BigDecimal("0.48440594"));
+        assertEquals(target.calculateConfidence(now.minus(7, DAYS), now), new BigDecimal("0.45035231"));
+        assertEquals(target.calculateConfidence(now.minus(30, DAYS), now), new BigDecimal("0.41079350"));
+        assertEquals(target.calculateConfidence(now.minus(365, DAYS), now), new BigDecimal("0.34287180"));
+        assertEquals(target.calculateConfidence(now.minus(3650, DAYS), now), new BigDecimal("0.28028105"));
+        assertEquals(target.calculateConfidence(now.minus(365000, DAYS), now), new BigDecimal("0.15509954"));
 
     }
 
