0fec1282e0c6f2288184193d49911a8651e92075
Takanori Takase <scm@after-sunrise.com>
2017-09-30 20:35:20 +0900

    Added composite estimator.

---
+++ src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/composite/CompositeService.java
@@ -0,0 +1,80 @@
+package com.after_sunrise.cryptocurrency.cryptotrader.service.composite;
+
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context.Key;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Request;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Service;
+import com.after_sunrise.cryptocurrency.cryptotrader.service.estimator.Estimators;
+import com.after_sunrise.cryptocurrency.cryptotrader.service.estimator.MidEstimator;
+import com.google.common.annotations.VisibleForTesting;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.ONE;
+import static org.apache.commons.lang3.StringUtils.split;
+import static org.apache.commons.lang3.StringUtils.trimToEmpty;
+
+/**
+ * @author takanori.takase
+ * @version 0.0.1
+ */
+public interface CompositeService extends Service {
+
+    String ID = "composite";
+
+    class CompositeMidEstimator extends MidEstimator {
+
+        private static final String DELIMITER = "|";
+
+        private static final String INSTRUMENT = ":";
+
+        @Override
+        public Context.Key getKey(Request request) {
+            return Estimators.getKey(request, ID);
+        }
+
+        @VisibleForTesting
+        protected Estimation estimate(Context context, Key key) {
+            return super.estimate(context, key);
+        }
+
+        @Override
+        public Estimation estimate(Context context, Request request) {
+
+            Key compositeKey = getKey(request);
+
+            Key.KeyBuilder builder = Key.build(compositeKey);
+
+            BigDecimal price = ONE;
+
+            BigDecimal confidence = ONE;
+
+            for (String target : split(trimToEmpty(compositeKey.getInstrument()), DELIMITER)) {
+
+                String[] kv = split(target, INSTRUMENT, 2);
+
+                if (kv.length != 2) {
+                    continue;
+                }
+
+                Key key = builder.site(kv[0]).instrument(kv[1]).build();
+
+                Estimation estimation = estimate(context, key);
+
+                if (estimation == null || estimation.getPrice() == null || estimation.getConfidence() == null) {
+                    return BAIL;
+                }
+
+                price = price.multiply(estimation.getPrice());
+
+                confidence = confidence.multiply(estimation.getConfidence());
+
+            }
+
+            return Estimation.builder().price(price).confidence(confidence).build();
+
+        }
+
+    }
+
+}
---
+++ src/test/java/com/after_sunrise/cryptocurrency/cryptotrader/service/composite/CompositeServiceTest.java
@@ -0,0 +1,78 @@
+package com.after_sunrise.cryptocurrency.cryptotrader.service.composite;
+
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context.Key;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Estimator.Estimation;
+import com.after_sunrise.cryptocurrency.cryptotrader.framework.Request;
+import com.after_sunrise.cryptocurrency.cryptotrader.service.composite.CompositeService.CompositeMidEstimator;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import static com.after_sunrise.cryptocurrency.cryptotrader.service.composite.CompositeService.ID;
+import static java.math.BigDecimal.ONE;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author takanori.takase
+ * @version 0.0.1
+ */
+public class CompositeServiceTest {
+
+    @Test
+    public void testCompositeMidEstimator() {
+
+        Instant now = Instant.now();
+        Request request = Request.builder().site("ss").instrument("si").currentTime(now).build();
+        Context context = mock(Context.class);
+
+        BigDecimal p1 = new BigDecimal("0.1");
+        BigDecimal p2 = new BigDecimal("0.2");
+        BigDecimal p3 = new BigDecimal("0.3");
+
+        BigDecimal c1 = new BigDecimal("0.9");
+        BigDecimal c2 = new BigDecimal("0.8");
+        BigDecimal c3 = new BigDecimal("0.7");
+
+        Estimation e1 = Estimation.builder().price(p1).confidence(c1).build();
+        Estimation e2 = Estimation.builder().price(p2).confidence(c2).build();
+        Estimation e3 = Estimation.builder().price(p3).confidence(c3).build();
+
+        Key k0 = Key.builder().site(ID).instrument("a:x|b:y|c:z|d:|e|").timestamp(now).build();
+        Key k1 = Key.builder().site("a").instrument("x").timestamp(now).build();
+        Key k2 = Key.builder().site("b").instrument("y").timestamp(now).build();
+        Key k3 = Key.builder().site("c").instrument("z").timestamp(now).build();
+
+        CompositeMidEstimator target = spy(new CompositeMidEstimator());
+        doReturn(k0).when(target).getKey(request);
+        doReturn(e1).when(target).estimate(context, k1);
+        doReturn(e2).when(target).estimate(context, k2);
+        doReturn(e3).when(target).estimate(context, k3);
+
+        Estimation result = target.estimate(context, request);
+        assertEquals(result.getPrice(), new BigDecimal("0.006"));
+        assertEquals(result.getConfidence(), new BigDecimal("0.504"));
+
+        // Null confidence
+        doReturn(Estimation.builder().price(ONE).build()).when(target).estimate(context, k2);
+        result = target.estimate(context, request);
+        assertEquals(result.getPrice(), null);
+        assertEquals(result.getConfidence(), new BigDecimal("0"));
+
+        // Null price
+        doReturn(Estimation.builder().confidence(ONE).build()).when(target).estimate(context, k2);
+        result = target.estimate(context, request);
+        assertEquals(result.getPrice(), null);
+        assertEquals(result.getConfidence(), new BigDecimal("0"));
+
+        // Null estimation
+        doReturn(null).when(target).estimate(context, k2);
+        result = target.estimate(context, request);
+        assertEquals(result.getPrice(), null);
+        assertEquals(result.getConfidence(), new BigDecimal("0"));
+
+    }
+
+}
