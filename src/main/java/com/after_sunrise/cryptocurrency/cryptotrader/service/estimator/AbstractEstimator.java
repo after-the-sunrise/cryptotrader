f4294ae36c22eb23ae4614a067a827e05bd8975d
Takanori Takase <scm@after-sunrise.com>
2017-10-28 18:28:39 +0900

    Moved common methods.

--- src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/AbstractEstimator.java
+++ src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/AbstractEstimator.java
@@ -3,19 +3,13 @@ package com.after_sunrise.cryptocurrency.cryptotrader.service.estimator;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context.Key;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Estimator;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Request;
-import com.after_sunrise.cryptocurrency.cryptotrader.framework.Trade;
 import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.configuration2.ImmutableConfiguration;
 
 import javax.inject.Inject;
 import java.math.BigDecimal;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.*;
-import java.util.Map.Entry;
 
 import static java.math.BigDecimal.ZERO;
-import static java.math.RoundingMode.HALF_UP;
 
 /**
  * @author takanori.takase
@@ -79,105 +73,6 @@ public abstract class AbstractEstimator implements Estimator {
 
     protected Key getKey(Request request) {
         return Key.from(request);
-    }
-
-    protected NavigableMap<Instant, BigDecimal> collapsePrices(List<Trade> values,
-                                                               Duration interval, Instant from, Instant to) {
-
-        NavigableMap<Instant, BigDecimal[]> collapsed = new TreeMap<>();
-
-        for (long i = from.toEpochMilli(); i < to.toEpochMilli(); i += interval.toMillis()) {
-
-            Instant instant = Instant.ofEpochMilli(i);
-
-            collapsed.put(instant, new BigDecimal[2]); // [size, notional]
-
-        }
-
-        Optional.ofNullable(values).orElse(Collections.emptyList()).stream()
-                .filter(Objects::nonNull)
-                .filter(t -> t.getTimestamp() != null)
-                .filter(t -> t.getTimestamp().isAfter(from.minus(interval)))
-                .filter(t -> t.getTimestamp().isBefore(to))
-                .filter(t -> t.getPrice() != null)
-                .filter(t -> t.getSize() != null).forEach(t -> {
-
-            Instant timestamp = t.getTimestamp();
-
-            Entry<Instant, BigDecimal[]> entry = collapsed.ceilingEntry(timestamp);
-
-            if (entry == null) {
-                return;
-            }
-
-            BigDecimal[] elements = entry.getValue();
-            elements[0] = (elements[0] == null ? ZERO : elements[0]).add(t.getSize());
-            elements[1] = (elements[1] == null ? ZERO : elements[1]).add(t.getSize().multiply(t.getPrice()));
-
-        });
-
-        NavigableMap<Instant, BigDecimal> prices = new TreeMap<>();
-
-        BigDecimal previous = null;
-
-        for (Entry<Instant, BigDecimal[]> entry : collapsed.entrySet()) {
-
-            BigDecimal[] elements = entry.getValue();
-
-            BigDecimal current = previous;
-
-            if (elements[0] != null && elements[0].signum() != 0) {
-                current = elements[1].divide(elements[0], SCALE, HALF_UP);
-            }
-
-            prices.put(entry.getKey(), current);
-
-            previous = current;
-
-        }
-
-        return prices;
-
-    }
-
-    protected NavigableMap<Instant, BigDecimal> calculateReturns(SortedMap<Instant, BigDecimal> prices) {
-
-        if (prices == null) {
-            return Collections.emptyNavigableMap();
-        }
-
-        NavigableMap<Instant, BigDecimal> returns = new TreeMap<>();
-
-        List<Entry<Instant, BigDecimal>> entries = new ArrayList<>(prices.entrySet());
-
-        for (int i = 1; i < entries.size(); i++) {
-
-            BigDecimal p0 = entries.get(i - 1).getValue();
-
-            BigDecimal p1 = entries.get(i).getValue();
-
-            BigDecimal value = null;
-
-            if (p0 != null && p1 != null) {
-
-                double diff = Math.log(p1.doubleValue() / p0.doubleValue());
-
-                if (Double.isFinite(diff)) {
-
-                    value = BigDecimal.valueOf(diff);
-
-                    value = value.setScale(SCALE, HALF_UP);
-
-                }
-
-            }
-
-            returns.put(entries.get(i).getKey(), value);
-
-        }
-
-        return returns;
-
     }
 
 }
--- src/test/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/AbstractEstimatorTest.java
+++ src/test/java/com/after_sunrise/cryptocurrency/cryptotrader/service/estimator/AbstractEstimatorTest.java
@@ -2,20 +2,15 @@ package com.after_sunrise.cryptocurrency.cryptotrader.service.estimator;
 
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Request;
-import com.after_sunrise.cryptocurrency.cryptotrader.framework.Trade;
 import org.apache.commons.configuration2.MapConfiguration;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.math.BigDecimal;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.math.BigDecimal.ONE;
 import static java.math.BigDecimal.TEN;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 
 /**
@@ -78,157 +73,6 @@ public class AbstractEstimatorTest {
             }
         }); // Exception
         assertEquals(target.getStringConfiguration("string", "b"), "b");
-
-    }
-
-    @Test
-    public void testCollapsePrices() throws Exception {
-
-        List<Trade> trades = new ArrayList<>();
-
-        for (int i = 1; i <= 20; i++) {
-            Trade t = mock(Trade.class);
-            when(t.getTimestamp()).thenReturn(Instant.ofEpochMilli(i + 11000));
-            when(t.getPrice()).thenReturn(BigDecimal.valueOf(i + 1000));
-            when(t.getSize()).thenReturn(BigDecimal.valueOf(i + 100));
-            trades.add(t);
-        }
-
-        Duration interval = Duration.ofMillis(4);
-        Instant fromTime = Instant.ofEpochMilli(10990);
-        Instant toTime = Instant.ofEpochMilli(11035);
-        NavigableMap<Instant, BigDecimal> result = target.collapsePrices(trades, interval, fromTime, toTime);
-        assertEquals(result.size(), 12);
-        assertEquals(result.remove(Instant.ofEpochMilli(10990)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(10994)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(10998)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11002)), new BigDecimal("1001.5024630542"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), new BigDecimal("1008.5115207373"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), new BigDecimal("1012.5111111111"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("1016.5107296137"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.size(), 0, result.toString());
-
-        // Remove a bucket
-        when(trades.get(6).getTimestamp()).thenReturn(Instant.ofEpochMilli(10986)); // Before
-        when(trades.get(7).getTimestamp()).thenReturn(Instant.ofEpochMilli(11035)); // After
-        when(trades.get(8).getPrice()).thenReturn(null);
-        when(trades.get(9).getSize()).thenReturn(null);
-        result = target.collapsePrices(trades, interval, fromTime, toTime);
-        assertEquals(result.size(), 12);
-        assertEquals(result.remove(Instant.ofEpochMilli(10990)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(10994)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(10998)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11002)), new BigDecimal("1001.5024630542"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), new BigDecimal("1012.5111111111"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("1016.5107296137"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.size(), 0, result.toString());
-
-        // Include previous interval.
-        when(trades.get(6).getTimestamp()).thenReturn(Instant.ofEpochMilli(10987));
-        result = target.collapsePrices(trades, interval, fromTime, toTime);
-        assertEquals(result.size(), 12);
-        assertEquals(result.remove(Instant.ofEpochMilli(10990)), new BigDecimal("1007.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(10994)), new BigDecimal("1007.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(10998)), new BigDecimal("1007.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11002)), new BigDecimal("1001.5024630542"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), new BigDecimal("1012.5111111111"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("1016.5107296137"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.size(), 0, result.toString());
-
-        // Invalidate bucket
-        when(trades.get(6).getTimestamp()).thenReturn(null);
-        when(trades.get(9).getSize()).thenReturn(BigDecimal.ZERO);
-        result = target.collapsePrices(trades, interval, fromTime, toTime);
-        assertEquals(result.size(), 12);
-        assertEquals(result.remove(Instant.ofEpochMilli(10990)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(10994)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(10998)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11002)), new BigDecimal("1001.5024630542"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), new BigDecimal("1004.5119617225"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), new BigDecimal("1012.5111111111"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("1016.5107296137"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("1019.5020920502"));
-        assertEquals(result.size(), 0, result.toString());
-
-    }
-
-    @Test
-    public void testCalculateReturns() throws Exception {
-
-        SortedMap<Instant, BigDecimal> prices = new TreeMap<>();
-        prices.put(Instant.ofEpochMilli(11002), new BigDecimal("1001.5024630542"));
-        prices.put(Instant.ofEpochMilli(11006), new BigDecimal("1004.5119617225"));
-        prices.put(Instant.ofEpochMilli(11010), new BigDecimal("1008.5115207373"));
-        prices.put(Instant.ofEpochMilli(11014), new BigDecimal("1012.5111111111"));
-        prices.put(Instant.ofEpochMilli(11018), new BigDecimal("1016.5107296137"));
-        prices.put(Instant.ofEpochMilli(11022), new BigDecimal("1019.5020920502"));
-        prices.put(Instant.ofEpochMilli(11026), new BigDecimal("1019.5020920502"));
-        prices.put(Instant.ofEpochMilli(11030), new BigDecimal("1019.5020920502"));
-        prices.put(Instant.ofEpochMilli(11034), new BigDecimal("1019.5020920502"));
-
-        NavigableMap<Instant, BigDecimal> result = target.calculateReturns(prices);
-        assertEquals(result.size(), prices.size() - 1);
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("0.0030004779"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), new BigDecimal("0.0039736886"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), new BigDecimal("0.0039579919"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("0.0039424156"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("0.0029384536"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("0.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("0.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("0.0000000000"));
-        assertEquals(result.size(), 0, result.toString());
-
-        // Missing Price
-        prices.put(Instant.ofEpochMilli(11010), null);
-        result = target.calculateReturns(prices);
-        assertEquals(result.size(), prices.size() - 1);
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("0.0030004779"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("0.0039424156"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("0.0029384536"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("0.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("0.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("0.0000000000"));
-        assertEquals(result.size(), 0, result.toString());
-
-        // Zero Price
-        prices.put(Instant.ofEpochMilli(11010), new BigDecimal("0.0"));
-        result = target.calculateReturns(prices);
-        assertEquals(result.size(), prices.size() - 1);
-        assertEquals(result.remove(Instant.ofEpochMilli(11006)), new BigDecimal("0.0030004779"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11010)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11014)), null);
-        assertEquals(result.remove(Instant.ofEpochMilli(11018)), new BigDecimal("0.0039424156"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11022)), new BigDecimal("0.0029384536"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11026)), new BigDecimal("0.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11030)), new BigDecimal("0.0000000000"));
-        assertEquals(result.remove(Instant.ofEpochMilli(11034)), new BigDecimal("0.0000000000"));
-        assertEquals(result.size(), 0, result.toString());
-
-        // Null arg
-        assertEquals(target.calculateReturns(null).size(), 0);
 
     }
 
