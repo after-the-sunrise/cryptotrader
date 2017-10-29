a1b0a5176c7c2be94e0c3c1f31ab9b6e5e1908e6
Takanori Takase <scm@after-sunrise.com>
2017-10-29 18:00:05 +0900

    Added basis logging.

--- src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/framework/impl/AdviserImpl.java
+++ src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/framework/impl/AdviserImpl.java
@@ -6,12 +6,18 @@ import com.after_sunrise.cryptocurrency.cryptotrader.framework.Context;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Estimator.Estimation;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Request;
 import com.after_sunrise.cryptocurrency.cryptotrader.framework.Service;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import lombok.extern.slf4j.Slf4j;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.RoundingMode.HALF_UP;
 
 /**
  * @author takanori.takase
@@ -51,9 +57,38 @@ public class AdviserImpl implements Adviser {
 
         Advice advice = adviser.advise(context, request, estimation);
 
+        BigDecimal b = calculateBasis(estimation, advice, Advice::getBuyLimitPrice, ONE.negate());
+
+        BigDecimal s = calculateBasis(estimation, advice, Advice::getSellLimitPrice, ONE);
+
+        log.debug("Advice : [{} {}] bBasis=[{}] sBasis=[{}]", request.getSite(), request.getInstrument(), b, s);
+
         log.info("Advice : [{} {}] {}", request.getSite(), request.getInstrument(), advice);
 
         return Optional.ofNullable(advice).orElse(BAIL);
+
+    }
+
+    @VisibleForTesting
+    BigDecimal calculateBasis(Estimation estimation, Advice advice, Function<Advice, BigDecimal> f, BigDecimal m) {
+
+        if (estimation == null || advice == null) {
+            return null;
+        }
+
+        BigDecimal estimate = estimation.getPrice();
+
+        if (estimate == null || estimate.signum() == 0) {
+            return null;
+        }
+
+        BigDecimal price = f.apply(advice);
+
+        if (price == null) {
+            return null;
+        }
+
+        return price.divide(estimate, SCALE, HALF_UP).subtract(ONE).multiply(m);
 
     }
 
