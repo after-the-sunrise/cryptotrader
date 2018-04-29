61c2ed15ec7f1b598fe1d38231de579e7522f0c7
Takanori Takase <scm@after-sunrise.com>
2018-04-29 09:01:21 +0900

    Added Zaif services.

---
+++ src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/zaif/ZaifAgent.java
@@ -0,0 +1,15 @@
+package com.after_sunrise.cryptocurrency.cryptotrader.service.zaif;
+
+import com.after_sunrise.cryptocurrency.cryptotrader.service.template.TemplateAgent;
+
+/**
+ * @author takanori.takase
+ * @version 0.0.1
+ */
+public class ZaifAgent extends TemplateAgent implements ZaifService {
+
+    public ZaifAgent() {
+        super(ID);
+    }
+
+}
---
+++ src/main/java/com/after_sunrise/cryptocurrency/cryptotrader/service/zaif/ZaifInstructor.java
@@ -0,0 +1,15 @@
+package com.after_sunrise.cryptocurrency.cryptotrader.service.zaif;
+
+import com.after_sunrise.cryptocurrency.cryptotrader.service.template.TemplateInstructor;
+
+/**
+ * @author takanori.takase
+ * @version 0.0.1
+ */
+public class ZaifInstructor extends TemplateInstructor implements ZaifService {
+
+    public ZaifInstructor() {
+        super(ID);
+    }
+
+}
